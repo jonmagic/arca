0382bdb38b2d39720093dce8bf0a7d918aa7d054
Jonathan Hoyt <hoyt@github.com>
2015-07-13 23:30:06 -0700

    Update collector test to new implementation

--- test/lib/arca/collector_test.rb
+++ test/lib/arca/collector_test.rb
@@ -1,22 +1,31 @@
 require_relative "../../test_helper"
 
-# class Arca::CollectorTest < Minitest::Test
-#   def test_collects_callback_data
-#     callbacks = Ticket.arca_callback_data
-#
-#     # callback = callbacks[:callbacks][0]
-#     # assert_equal :after_save, callback[:callback_symbol]
-#     # assert_equal [:announce_save], callback[:args]
-#     # assert_match "test/fixtures/announcements.rb:4:in `block in included'", callback[:line]
-#     #
-#     # callback = callbacks[:callbacks][1]
-#     # assert_equal :before_save, callback[:callback_symbol]
-#     # assert_equal [:set_title], callback[:args]
-#     # assert_match "test/fixtures/ticket.rb:5:in `<class:Ticket>'", callback[:line]
-#     #
-#     # callback = callbacks[:callbacks][2]
-#     # assert_equal :before_save, callback[:callback_symbol]
-#     # assert_equal [:upcase_title, {:if=>:title_is_a_shout?}], callback[:args]
-#     # assert_match "test/fixtures/ticket.rb:6:in `<class:Ticket>'", callback[:line]
-#   end
-# end
+class Arca::CollectorTest < Minitest::Test
+  def test_collects_callback_data
+    callbacks = Ticket.arca_callback_data
+
+    callback = callbacks[:after_save][0]
+    assert_equal :after_save, callback[:callback_symbol]
+    assert_match "test/fixtures/announcements.rb", callback[:callback_file_path]
+    assert_equal 4, callback[:callback_line_number]
+    assert_equal :announce_save, callback[:target_symbol]
+    assert_nil callback[:conditional_symbol]
+    assert_nil callback[:conditional_target_symbol]
+
+    callback = callbacks[:before_save][0]
+    assert_equal :before_save, callback[:callback_symbol]
+    assert_match "test/fixtures/ticket.rb", callback[:callback_file_path]
+    assert_equal 5, callback[:callback_line_number]
+    assert_equal :set_title, callback[:target_symbol]
+    assert_nil callback[:conditional_symbol]
+    assert_nil callback[:conditional_target_symbol]
+
+    callback = callbacks[:before_save][1]
+    assert_equal :before_save, callback[:callback_symbol]
+    assert_match "test/fixtures/ticket.rb", callback[:callback_file_path]
+    assert_equal 6, callback[:callback_line_number]
+    assert_equal :upcase_title, callback[:target_symbol]
+    assert_equal :if, callback[:conditional_symbol]
+    assert_equal :title_is_a_shout?, callback[:conditional_target_symbol]
+  end
+end
