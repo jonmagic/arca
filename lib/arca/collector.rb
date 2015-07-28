112d7bf1b5ffb9f673c6a379b296348cd1608386
Jonathan Hoyt <hoyt@github.com>
2015-07-28 15:42:56 -0700

    Use base instead of self for clarity

--- lib/arca/collector.rb
+++ lib/arca/collector.rb
@@ -86,7 +86,7 @@ module Arca
             end
 
             # Bind the callback method to self and call it with args.
-            callback_method.bind(self).call(*args)
+            callback_method.bind(base).call(*args)
           end
         end
       end
